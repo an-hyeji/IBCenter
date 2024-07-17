75b3e115a22a148cbdfec9218a3f34574804550b
an-hyeji <coolzk@naver.com>
2024-07-17 16:56:14 +0900

    feat: create modal

---
+++ assets/js/modal.js
@@ -0,0 +1,17 @@
+const modal = document.querySelector('.modal');
+const btn = document.querySelector('.modal-button');
+const closeBtn = document.querySelector('.close');
+
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+closeBtn.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
