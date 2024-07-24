1caee02a552bfb5955c1abadace78a23fb179de6
an-hyeji <coolzk@naver.com>
2024-07-24 11:18:54 +0900

    fix: modify heater

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,3 +66,32 @@ const sideMenu = document.querySelector('.side');
 clickbtn.addEventListener('click', function () {
   sideMenu.classList.toggle('hide');
 });
+
+// 사이드메뉴 토글
+const listTitles = document.querySelectorAll('.list-title');
+
+listTitles.forEach((listTitle) => {
+  listTitle.addEventListener('click', function () {
+    const currentSubMenu = this.querySelector('.sub-menu');
+
+    listTitles.forEach((title) => {
+      if (title !== this) {
+        const otherSubMenu = title.querySelector('.sub-menu');
+        if (otherSubMenu.classList.contains('active')) {
+          otherSubMenu.classList.remove('active');
+          title.classList.remove('open');
+        }
+      }
+    });
+
+    currentSubMenu.classList.toggle('active');
+    this.classList.toggle('open');
+  });
+
+  const subMenu = listTitle.querySelector('.sub-menu');
+  if (subMenu) {
+    subMenu.addEventListener('click', (e) => {
+      e.stopPropagation();
+    });
+  }
+});
