5b047447413beb328adb6b961011fb40f02ebf31
an-hyeji <coolzk@naver.com>
2024-08-01 13:25:04 +0900

    feat: create calendar detail model

--- calendar/js/calendar.js
+++ calendar/js/calendar.js
@@ -4,10 +4,7 @@ const popupContent = document.querySelector('.popup-content');
 const closeBtn = document.querySelector('.close');
 
 tds.forEach((td) => {
-  td.addEventListener('click', (event) => {
-    const rect = td.getBoundingClientRect();
-    popup.style.top = `${rect.bottom + window.scrollY}px`;
-    popup.style.left = `${rect.left + window.scrollX}px`;
+  td.addEventListener('click', () => {
     popup.style.display = 'block';
   });
 });
@@ -16,8 +13,8 @@ closeBtn.addEventListener('click', () => {
   popup.style.display = 'none';
 });
 
-window.addEventListener('click', (event) => {
-  if (event.target == popup) {
+window.addEventListener('click', (e) => {
+  if (e.target == popup) {
     popup.style.display = 'none';
   }
 });
