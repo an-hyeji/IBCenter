47a1cc3b9da0a0ff5cb798a8d3e8ac0e94bd1971
an-hyeji <coolzk@naver.com>
2024-08-07 16:57:35 +0900

    feat: create navigation-investment-management

---
+++ assets/js/navigation.js
@@ -0,0 +1,15 @@
+function setTopActive(element) {
+  const topMenuItems = document.querySelectorAll('.top nav ul li');
+  topMenuItems.forEach(function (item) {
+    item.classList.remove('active');
+  });
+  element.classList.add('active');
+}
+
+function setListActive(element) {
+  const listMenuItems = document.querySelectorAll('.list-box .list ul li');
+  listMenuItems.forEach(function (item) {
+    item.classList.remove('active');
+  });
+  element.classList.add('active');
+}
