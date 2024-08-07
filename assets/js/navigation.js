8ffbaa697a0a452c3a821c962235ef856af50865
an-hyeji <coolzk@naver.com>
2024-08-07 18:09:17 +0900

    fix: modify navigation

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -6,10 +6,10 @@ function setTopActive(element) {
   element.classList.add('active');
 }
 
-function setListActive(element) {
-  const listMenuItems = document.querySelectorAll('.list-box .list ul li');
-  listMenuItems.forEach(function (item) {
-    item.classList.remove('active');
-  });
-  element.classList.add('active');
-}
+// function setListActive(element) {
+//   const listMenuItems = document.querySelectorAll('.list-box .list ul li');
+//   listMenuItems.forEach(function (item) {
+//     item.classList.remove('active');
+//   });
+//   element.classList.add('active');
+// }
