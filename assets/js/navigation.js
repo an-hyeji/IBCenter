463c83feaa37427b64508cbb78982348984c6d3c
an-hyeji <coolzk@naver.com>
2024-08-08 11:13:34 +0900

    fix: modify text

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -13,3 +13,24 @@ function setTopActive(element) {
 //   });
 //   element.classList.add('active');
 // }
+
+const modal = document.querySelector('.bookmark-modal');
+const btn = document.querySelector('.bookmark-button');
+const closeBtn = document.querySelector('.modal-close');
+
+btn.onclick = function () {
+  modal.style.display = 'block';
+  document.body.style.overflow = 'hidden';
+};
+
+closeBtn.onclick = function () {
+  modal.style.display = 'none';
+  document.body.style.overflow = 'auto';
+};
+
+window.onclick = function (e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+    document.body.style.overflow = 'auto';
+  }
+};
